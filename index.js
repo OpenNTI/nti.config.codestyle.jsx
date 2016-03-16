56776eda67c2975c36801880edb622b2fa33e2b9
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-16 11:38:32 -0500

    clean up and document rules

--- index.js
+++ index.js
@@ -16,16 +16,45 @@ module.exports = {
 	},
 
 	"rules": {
+		//We standarize on using double quotes on JSX props since they look like HTML attributes.
+		//See: http://eslint.org/docs/rules/jsx-quotes
 		"jsx-quotes": [1, "prefer-double"],
 
-		"react/jsx-no-bind": [1, {"ignoreRefs": true}],
+		//Standarize on unary/valueless attributes for constant boolean props.
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
 		"react/jsx-boolean-value": 1,
-		"react/jsx-sort-props": 0,
-		"react/no-danger": 0,
-		"react/no-multi-comp": 0,
+
+		//Show an error when you need to supply a key for react to identify array elements.
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
+		"react/jsx-key": 2,
+
+		//Using bind() or arrow functions on props always trigger a prop-change.
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
+		"react/jsx-no-bind": [1, {"ignoreRefs": true}],
+
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
+		"react/jsx-no-duplicate-props": 1,
+
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
+		"react/jsx-pascal-case": 1,
+
+		//React has said in a blog post the string ref is legacy. (which we interpret to mean don't use anymore)
+		//See: http://facebook.github.io/react/docs/more-about-refs.html#the-ref-string-attribute
 		"react/no-string-refs": 1,
+
+		//React has several ways to define a React Component. The simplest and lightest weight form is
+		//the stateless-function. If the component you are righting can be a stateless-function, it should be.
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
 		"react/prefer-stateless-function": 1,
+
+		//All child-less elements should self close. ex:  <div></div> should be <div/>
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
 		"react/self-closing-comp": 2,
-		"react/wrap-multilines": 2
+
+		//JSX should always be wrapped in parentheses... especically when multiline.
+		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
+		"react/wrap-multilines": [2, {declaration: true, assignment: true, return: true}]
+
+		//"react/jsx-handler-names": 1
 	}
 };
