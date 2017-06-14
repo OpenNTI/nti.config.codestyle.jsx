07965120ef5977d1b03079da5cce910a01af2127
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-14 10:55:16 -0500

    cleanup, use live react version, drop rules already defined in bases

--- index.js
+++ index.js
@@ -1,66 +1,55 @@
 module.exports = {
-	"extends": [
-		"eslint-config-nti-codestyle-js",
-		"plugin:react/recommended"
+	extends: [
+		'eslint-config-nti-codestyle-js',
+		'plugin:react/recommended'
 	],
 
-	"plugins": [
-		"react"
-	],
-
-	"settings": {
-		"import/extensions": ['.js', '.jsx'],
-		"import/resolver": "webpack",
-		"react": {
-			"pragma": "React",
-			"version": "15.5.0"
+	settings: {
+		'import/extensions': ['.js', '.jsx'],
+		'import/resolver': 'webpack',
+		'react': {
+			pragma: 'React',
+			version: require('react/package.json').version
 		}
 	},
 
-	"rules": {
-		"import/no-extraneous-dependencies": [2, {"devDependencies": true}],
+	rules: {
+		'import/no-extraneous-dependencies': ['error', {'devDependencies': true}],
 		
 		//We standarize on using double quotes on JSX props since they look like HTML attributes.
 		//See: http://eslint.org/docs/rules/jsx-quotes
-		"jsx-quotes": [1, "prefer-double"],
+		'jsx-quotes': ['warn', 'prefer-double'],
 
 		//Standarize on unary/valueless attributes for constant boolean props.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
-		"react/jsx-boolean-value": 1,
-
-		//Show an error when you need to supply a key for react to identify array elements.
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
-		"react/jsx-key": 2,
+		'react/jsx-boolean-value': 'warn',
 
 		//Using bind() or arrow functions on props always trigger a prop-change.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
-		"react/jsx-no-bind": 1,
+		'react/jsx-no-bind': 'error',
 		
-		// "react/no-children-prop": 1,
-
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-duplicate-props.md
-		"react/jsx-no-duplicate-props": 1,
+		// 'react/no-children-prop': 'warn',
 
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
-		"react/jsx-pascal-case": [1, { "allowAllCaps": true }],
+		'react/jsx-pascal-case': ['warn', { 'allowAllCaps': true }],
 
 		//React has said in a blog post the string ref is legacy. (which we interpret to mean don't use anymore)
 		//See: http://facebook.github.io/react/docs/more-about-refs.html#the-ref-string-attribute
-		"react/no-string-refs": 1,
+		"react/no-string-refs": 'warn',
 
 		//React has several ways to define a React Component. The simplest and lightest weight form is
 		//the stateless-function. If the component you are writing can be a stateless-function, it should be.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
-		"react/prefer-stateless-function": [1, { "ignorePureComponents": true }],
+		"react/prefer-stateless-function": ['warn', { "ignorePureComponents": true }],
 
 		//All child-less elements should self close. ex:  <div></div> should be <div/>
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
-		"react/self-closing-comp": 2,
+		"react/self-closing-comp": 'error',
 
 		//JSX should always be wrapped in parentheses... especically when multiline.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
-		"react/jsx-wrap-multilines": [2, {declaration: true, assignment: true, return: true}]
+		"react/jsx-wrap-multilines": ['error', {declaration: true, assignment: true, return: true}]
 
-		//"react/jsx-handler-names": 1
+		//"react/jsx-handler-names": 'warn'
 	}
 };
