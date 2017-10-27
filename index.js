a4ab32a7bdd06aebd7992b7be7c84ff868ffd26a
Jonathan Grimes <jsg2021@gmail.com>
2017-10-27 15:03:53 -0500

    cleanup some out dated rules

--- index.js
+++ index.js
@@ -36,19 +36,8 @@ module.exports = {
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
 		'react/jsx-no-bind': 'error',
 		
-		// 'react/no-children-prop': 'warn',
-
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
 		'react/jsx-pascal-case': ['warn', { 'allowAllCaps': true }],
-
-		//React has said in a blog post the string ref is legacy. (which we interpret to mean don't use anymore)
-		//See: http://facebook.github.io/react/docs/more-about-refs.html#the-ref-string-attribute
-		"react/no-string-refs": 'warn',
-
-		//React has several ways to define a React Component. The simplest and lightest weight form is
-		//the stateless-function. If the component you are writing can be a stateless-function, it should be.
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
-		"react/prefer-stateless-function": ['warn', { "ignorePureComponents": true }],
 
 		//All child-less elements should self close. ex:  <div></div> should be <div/>
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
@@ -57,7 +46,5 @@ module.exports = {
 		//JSX should always be wrapped in parentheses... especically when multiline.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
 		"react/jsx-wrap-multilines": ['error', {declaration: true, assignment: true, return: true}]
-
-		//"react/jsx-handler-names": 'warn'
 	}
 };
